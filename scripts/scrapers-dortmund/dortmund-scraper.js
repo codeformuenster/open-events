18cdb4a35c69a9555bc19e4c9c8c56458412eb08
Marcel Reschke <hello@marcelreschke.com>
2015-06-01 16:15:37 +0200

    Change output from YAML to JSON

--- scripts/scrapers-dortmund/dortmund-scraper.js
+++ scripts/scrapers-dortmund/dortmund-scraper.js
@@ -2,22 +2,56 @@ var cheerio = require('cheerio');
     request = require('request');
     url = 'http://www.fzw.de/programm/'
 
+
+
 request(url, function (error, response, body) {
 	if (!error) {
   	var $ = cheerio.load(body);
-    bigBox = $('.bigBox').html();
-    $ = cheerio.load(bigBox);
-  	$('div[style="position: relative; background: url(img/blank.gif);"]').each(function() {
+  	$('div[style="position: relative; background: url(img/blank.gif);"] > a').each(function(i, element) {
+      eventUrl = "http://www.fzw.de/"+ $(this).attr('href');
+      request(eventUrl, function (error, response, body) {
+      	if (!error) {
+          var $ = cheerio.load(body);
+          var eventDate = $('.bigBox > h2:first-of-type').text();
+          eventDate = eventDate.substring(4);
+          var eventDetails = $('.bigBox > h2:nth-of-type(3)').text();
+          var eventDetailsRegEx = /Beginn: (\d\d.\d\d)/;
+          var eventDetailsResult = eventDetails.match(eventDetailsRegEx);
+          if (eventDetailsResult !== null) var eventStartTime = eventDetailsResult[1];
+          var eventName = $('.bigBox > h3:first-of-type').text().trim();
+          $('.bigBox > div:nth-of-type(2) > *').each(function() {
+            var content = $(this).text();
+            $(this).replaceWith(content);
+          });
+          var eventDescription = ($('.bigBox > div:nth-of-type(2)').text().trim());
+          var eventJSON = {
+            "@context": "http://schema.org",
+            "@type": "Event",
+            "name": eventName,
+            "description": eventDescription,
+            "location": {
+              "@type": "Place",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress" : "Ritterstr. 20",
+                "addressLocality" : "Dortmund",
+                "postalCode" : "44137"
+                }
+              },
+            "startDate": eventDate + eventStartTime,
+            "url": eventUrl
+          }
 
-      eventBlob = $(this).html();
-      $ = cheerio.load(eventBlob);
-      console.log($('table > tr > td > h3').text().trim());
-      console.log($('table > tr > td[style="width: 85px; vertical-align: top;"] > h2').text().trim());
-      console.log($('table > tr > td > span').text().trim());
-      console.log($('.programEventInfo > table > tr > td:last-child').text().trim());
-      console.log("---")
+          console.log(JSON.stringify(eventJSON));
+        } else {
+      		console.log("Event HTTP Request fehlgeschlagen: " + error);
+      	}
+      });
     });
   } else {
-		console.log("HTTP Request fehlgeschlagen: " + error);
+		console.log("Übersicht HTTP Request fehlgeschlagen: " + error);
 	}
+
+
+
 });
